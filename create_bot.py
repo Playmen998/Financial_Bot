3d565baa788f12f72ff29cd3b1f8d6cf83106d34
Egor <lavrushkinkin@gmail.com>
2022-12-31 15:48:42 +0300

    edit password

--- create_bot.py
+++ create_bot.py
@@ -1,6 +1,9 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from config import TOKEN
+#from config import TOKEN
+import os
+
+TOKEN = os.getenv("TOKEN")
 
 storage = MemoryStorage()
 bot = Bot(TOKEN)
